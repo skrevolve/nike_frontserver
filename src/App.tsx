94becdea00489cee514e470b90cd8ad426677499
blackpeng15 <77031630+blackpeng15@users.noreply.github.com>
2022-04-08 10:25:14 +0900

    Sidebar1차작업_220428 (#80)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div>!!!!!!!!!!!!!!!나이키 메인화면 수정중!!!!!!!!!!!!!</div>
+      <Sidebar />
+      {/* <div>!!!!!!!!!!!!!!!나이키 메인화면 수정중!!!!!!!!!!!!!</div> */}
       <Footer />
     </div>
   );
