1974715b9453d2021c96b57e03fead21c4e724b0
blackpeng15 <blackpeng15@naver.com>
2022-04-11 16:17:09 +0900

    [#87]회원가입추가_220428

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom';
 import "./App.css";
 import NR_page from "./pages/NR_page";
 import Login from "./components/Login";
+import Sighup from "./components/Sighup";
 import Notice from "./components/Notice";
 
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/Login" element={<Login/>} />
+        <Route path="/Sighup" element={<Sighup/>} />
         <Route path="/Notice" element={<Notice/>} />
         <Route path="/NR_page" element={<NR_page/>} />
       </Routes>
