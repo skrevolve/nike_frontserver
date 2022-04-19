a841fbb1d304af8be5a1046ecd17cd1025701b16
blackpeng15 <77031630+blackpeng15@users.noreply.github.com>
2022-04-19 15:10:04 +0900

    [#127]마이페이지생성_220419 (#128)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 import NrPage from "./pages/NrPage";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Notice from "./components/Notice";
+import Mypage from "./components/Mypage";
 import Customer from "./pages/Customer";
 import Main from "./pages/Main";
 
@@ -15,6 +17,9 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
+        <Route path="/Notice" element={<Notice />} />
+        <Route path="/Mypage" element={<Mypage />} />
+        <Route path="/Nrpage" element={<Nrpage />} />
         <Route path="/Customer" element={<Customer />} />
         <Route path="/NrPage" element={<NrPage />} />
       </Routes>
